ac6bc7dbdd812939880149b04a0576afe34cb42b
codedna <parkerpunj@gmail.com>
2026-08-22 00:28:32 +0530

    FIX: fixed bg banding issue by replacing the css with an identical image

--- lib/classes/background-classes.ts
+++ lib/classes/background-classes.ts
@@ -4,7 +4,8 @@
 //Background for main area and history sidebar
 export enum BACKGROUNDS {
   original_mud_bg = "bg-[#0f0f0f]",
-  blue_bg = "bg-[radial-gradient(circle_at_center,#11151c_20%,#0b0f15_45%,#070a0f_100%)]",
+  // blue_bg = "bg-[radial-gradient(circle_at_center,#11151c_20%,#0b0f15_45%,#070a0f_100%)]",
+  blue_bg = "bg-[url('/images/dark-radial-bg.png')] bg-cover bg-center bg-no-repeat",
 }
 //Background for Upload area only
 export enum UPLOAD_AREA_BACKGROUNDS {
